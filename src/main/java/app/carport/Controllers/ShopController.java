cd45d258423a66e4969246681323078355e25438
Magnewei <magnewei@icloud.com>
2024-05-02 15:39:26 +0200

    Removed test class

--- src/main/java/app/carport/Controllers/ShopController.java
+++ src/main/java/app/carport/Controllers/ShopController.java
@@ -1,7 +1,12 @@
 package app.carport.Controllers;
 
+import app.carport.Entities.Carport;
 import app.carport.Persistence.ConnectionPool;
+import app.carport.SVG.CarportSVG;
 import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+import java.util.Locale;
 
 public class ShopController {
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
@@ -21,5 +26,18 @@ public class ShopController {
 
     public static void orderCarport(ConnectionPool connectionPool) {
 
+    }
+
+    public static void showOrder(Context ctx, Carport carport) {
+
+        // TODO: Sørg for at størrelsesforhold er passende til HTML-siden.
+        // Tag potentielt højde for skærmstørrelse samt skaler bredde og højde til passende mål.
+        int height = carport.getHeight();
+        int width = carport.getWidth();
+
+        Locale.setDefault(new Locale("US"));
+        CarportSVG svg = new CarportSVG(width, height);
+        ctx.attribute("svg", svg.toString());
+        ctx.render("SVGTest.html");
     }
 }
