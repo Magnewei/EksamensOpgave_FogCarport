ff0b533fdb34c78c12447e781b1e8b6cbe915afa
Magnus N <143735701+Magnewei@users.noreply.github.com>
2024-05-02 18:50:36 +0200

    Merge pull request #33
    * Fix ConnectionPool getInstance method.
    
    * Merge remote-tracking branch 'origin/master'
    
    * Merge remote-tracking branch 'origin/master'
    
    * Merge remote-tracking branch 'origin/master'
    
    * Added classes and boilerplate for preliminary methods.
    
    * Merge remote-tracking branch 'origin/master'
    
    * Merge remote-tracking branch 'origin/master'
    
    * Merge remote-tracking branch 'origin/master'
    
    * Merge remote-tracking branch 'origin/master'
    
    * Added classes and boilerplate for preliminary methods.
    
    * Changed pom.xml
    
    * Added boilerplate SVG code
    
    * Changed boilerplate + clean up
    
    * Removed test class
    
    * Removed test class
    
    * Added half baked SVG

---
+++ src/main/java/app/carport/SVG/CarportSVG.java
@@ -0,0 +1,29 @@
+package app.carport.SVG;
+
+public class CarportSVG {
+    private final SVGDrawer carportSvg;
+
+    public CarportSVG(int width, int length) {
+        carportSvg = new SVGDrawer(0, 0, "0 0 855 690", "75%" );
+        carportSvg.addRectangle(0,0,600, 780, "stroke-width:1px; stroke:#000000; fill: #ffffff");
+        addBeams();
+        addRafters();
+    }
+
+    // TODO: Fix konstanter i metoderne til at være baseret på width og height.
+    private void addBeams() {
+        carportSvg.addRectangle(0,35,4.5, 780, "stroke-width:1px; stroke:#000000; fill: #ffffff");
+        carportSvg.addRectangle(0,565,4.5, 780, "stroke-width:1px; stroke:#000000; fill: #ffffff");
+    }
+
+    private void addRafters() {
+        for (double i = 0; i < 780; i+= 55.714) {
+            carportSvg.addRectangle(i, 0.0, 600, 4.5,"stroke:#000000; fill: #ffffff" );
+        }
+    }
+
+    @Override
+    public String toString() {
+        return carportSvg.toString();
+    }
+}
