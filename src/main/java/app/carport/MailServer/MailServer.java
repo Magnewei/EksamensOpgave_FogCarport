305d6fb460bda5d3980f4f967f3b847792bb0dcf
Magnewei <magnewei@icloud.com>
2024-05-04 14:46:16 +0200

    Mail fixed

--- src/main/java/app/carport/MailServer/MailServer.java
+++ src/main/java/app/carport/MailServer/MailServer.java
@@ -1,11 +1,59 @@
 package app.carport.MailServer;
 
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Email;
+import com.sendgrid.helpers.mail.objects.Personalization;
+
+import java.io.IOException;
+
 public class MailServer {
+    private final static String API_KEY = System.getenv("SENDGRID_API_KEY");
 
-    // Form key: d-5b0856a83de84ed0b5825176c7fb50a6
-    // API key: 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
-    // API key short: m3u7ZCFKRNGIy91Aq7OEBw
+    public static boolean sendMail(String to, String name, String zip) {
+        // Used for return statement;
+        int responseCode = 0;
 
-    String API_KEY = System.getenv("SENDGRID_API_KEY");
+        // Get api key
+        SendGrid sg = new SendGrid(API_KEY);
 
+        // Email that we're sending our company mail from:
+        String email = "fogprojekt@icloud.com";
+        Email from = new Email(email);
+        from.setName("Johannes Fog Byggemarked");
+        Mail mail = new Mail();
+        mail.setFrom(from);
+        Personalization personalization = new Personalization();
+
+        // Instantiate customer details into a mail.
+        personalization.addTo(new Email(to));
+        personalization.addDynamicTemplateData("name", name);
+        personalization.addDynamicTemplateData("email", to);
+        personalization.addDynamicTemplateData("zip", zip);
+        mail.addPersonalization(personalization);
+        mail.addCategory("carportapp");
+
+        // Send mail
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+
+            // TemplateID is corresponding with the template created on SendGrid
+            mail.templateId = "d-5b0856a83de84ed0b5825176c7fb50a6";
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+
+            // Get response code for return statement.
+            responseCode = response.getStatusCode();
+
+        } catch (IOException e) {
+            System.out.println("Error sending mail");
+            System.out.println(e.getMessage());
+        }
+        return responseCode == 202;
+    }
 }
--- src/main/java/app/carport/Main.java
+++ src/main/java/app/carport/Main.java
@@ -1,16 +1,11 @@
 package app.carport;
 
 import app.carport.Controllers.*;
-import com.sendgrid.*;
-import com.sendgrid.helpers.mail.Mail;
-import com.sendgrid.helpers.mail.objects.Email;
-import com.sendgrid.helpers.mail.objects.Personalization;
+import app.carport.MailServer.MailServer;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
 import app.carport.Persistence.ConnectionPool;
 import app.carport.Thymeleaf.ThymeleafConfig;
-
-import java.io.IOException;
 
 public class Main {
     private static final String USER = System.getenv("JDBC_USER");
@@ -26,7 +21,7 @@ public class Main {
             config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
         }).start(7071);
 
-
+        MailServer.sendMail("magstener@gmail.com", "Magnus", "3000");
 
         // Routing
         app.get("/", ctx -> ctx.render("index.html"));
@@ -34,51 +29,5 @@ public class Main {
         LoginController.addRoutes(app, connectionPool);
         HeaderController.addRoutes(app, connectionPool);
         ShopController.addRoutes(app, connectionPool);
-
-        try {
-            testMail();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static void testMail() throws IOException {
-        String API_KEY = System.getenv("SENDGRID_API_KEY");
-        Email from = new Email("magnewei@icloud.com");
-        from.setName("Johannes Fog Byggemarked");
-        Mail mail = new Mail();
-        mail.setFrom(from);
-
-        Personalization personalization = new Personalization();
-
-        /* Erstat kunde@gmail.com, name, email og zip med egne værdier ****/
-        /* I test-fasen - brug din egen email, så du kan modtage beskeden */
-
-        personalization.addTo(new Email("magnewei@icloud.com"));
-        personalization.addDynamicTemplateData("name", "Anders And");
-        personalization.addDynamicTemplateData("email", "anders@and.dk");
-        personalization.addDynamicTemplateData("zip", "2100");
-        mail.addPersonalization(personalization);
-
-        mail.addCategory("carportapp");
-
-        SendGrid sg = new SendGrid(API_KEY);
-        Request request = new Request();
-        try {
-            request.setMethod(Method.POST);
-            request.setEndpoint("mail/send");
-
-            // indsæt dit skabelonid herunder
-            mail.templateId = "d-5b0856a83de84ed0b5825176c7fb50a6";
-            request.setBody(mail.build());
-            Response response = sg.api(request);
-            System.out.println(response.getStatusCode());
-            System.out.println(response.getBody());
-            System.out.println(response.getHeaders());
-
-        } catch (IOException ex) {
-            System.out.println("Error sending mail");
-            throw ex;
-        }
     }
 }
