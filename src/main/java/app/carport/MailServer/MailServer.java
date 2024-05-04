e5a4d401a68507e1940ab8fff039daec749b331b
Magnewei <magnewei@icloud.com>
2024-05-04 13:49:34 +0200

    Api keys

--- src/main/java/app/carport/MailServer/MailServer.java
+++ src/main/java/app/carport/MailServer/MailServer.java
@@ -4,4 +4,5 @@ public class MailServer {
 
     // Form key: d-5b0856a83de84ed0b5825176c7fb50a6
     // API key: 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
+    // API key short: m3u7ZCFKRNGIy91Aq7OEBw
 }
