cda56f1868720068b845e508b5dadab9b3220222
Anton Stengaard <afstengaard@gmail.com>
2024-05-05 19:47:48 +0200

    Carporte kan hentes, deres samlede pris kan udregnes

--- src/main/java/app/carport/Persistence/CarportMapper.java
+++ src/main/java/app/carport/Persistence/CarportMapper.java
@@ -1,6 +1,7 @@
 package app.carport.Persistence;
 
 import app.carport.Entities.Carport;
+import app.carport.Entities.Material;
 import app.carport.Entities.Order;
 import app.carport.Entities.User;
 import app.carport.Exceptions.DatabaseException;
@@ -9,7 +10,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Purpose:
@@ -18,27 +21,42 @@ import java.util.List;
  */
 public class CarportMapper {
     public static Carport getCarportByCarportId(int carportId, ConnectionPool connectionPool) throws DatabaseException {
-        String sql = "SELECT * FROM carport WHERE \"carportID\" = ?";
+        String sqlGetCarportData = "SELECT * FROM carport WHERE \"carportID\" = ?";
+        String sqlGetMaterials = "SELECT * FROM \"materialUsage\" WHERE \"carportID\" = ?";
+        int carportID = 0;
+        double length = 0;
+        double width = 0;
+        boolean hasShed = false;
 
-        try (Connection connection = connectionPool.getConnection(); PreparedStatement ps = connection.prepareStatement(sql);) {
+        try (Connection connection = connectionPool.getConnection(); PreparedStatement ps = connection.prepareStatement(sqlGetCarportData);) {
             ps.setInt(1, carportId);
             ResultSet rs = ps.executeQuery();
             if (rs.next()) {
-                int carportID = rs.getInt("carportID"); //
-                double length = rs.getDouble("length");
-                double width = rs.getDouble("width");
-                boolean hasShed = rs.getBoolean("hasShed");
-                //TODO: Få liste af materialer der skal bruges til en carport med
-                return new Carport(carportID, length, width, hasShed,new ArrayList<>());
+                carportID = rs.getInt("carportID"); //
+                length = rs.getDouble("length");
+                width = rs.getDouble("width");
+                hasShed = rs.getBoolean("hasShed");
 
+            }
+        }catch (SQLException e) {
+            throw new DatabaseException("Get carport data fejl", e.getMessage());
+        }
+
+        try(Connection connection = connectionPool.getConnection(); PreparedStatement ps = connection.prepareStatement(sqlGetMaterials);){
+            ps.setInt(1, carportId);
+            ResultSet rs = ps.executeQuery();
+            Map<Material,Integer> materialList = new HashMap<>();
+            while (rs.next()) {
+                int materialId = rs.getInt("materialID");
+                int quantity = rs.getInt("quantity");
+                materialList.put(MaterialMapper.getMaterialById(materialId,connectionPool),quantity);
+                return new Carport(carportID,length,width,hasShed,materialList);
             }
         } catch (SQLException e) {
             throw new DatabaseException("Get user fejl", e.getMessage());
         }
+
         return null;
     }
 
-    public Carport getCarportByOrder(Order order, ConnectionPool connectionPool) {
-        return null;
-    }
 }
