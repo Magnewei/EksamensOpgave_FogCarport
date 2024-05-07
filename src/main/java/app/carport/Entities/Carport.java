fb34e9093a454df4aa637ff9204c153fb0ea3ba0
Anton Stengaard <130669501+afstengaard@users.noreply.github.com>
2024-05-07 16:22:51 +0200

    Der er starten på en simpel udregner til styklisten. Den tager ikke højde for skur lige nu (#87)

--- src/main/java/app/carport/Entities/Carport.java
+++ src/main/java/app/carport/Entities/Carport.java
@@ -1,5 +1,12 @@
 package app.carport.Entities;
 
+import app.carport.Exceptions.DatabaseException;
+import app.carport.Persistence.ConnectionPool;
+import app.carport.Persistence.MaterialMapper;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -70,16 +77,54 @@ public class Carport {
         this.materialList = materialList;
     }
 
-    public double getTotalPrice(){
-        double totalPrice = 0;
-        for(Map.Entry<Material,Integer> entry:materialList.entrySet()){
-            Material material = entry.getKey();
-            int quantity = entry.getValue();
-            totalPrice += material.getPrice() * quantity;
+    public static Material getShortestSpærThatFits(List<Material> allMaterials, double length){
+        for(Material material : allMaterials){
+            if(material.getLength() == length){
+                return material;
+            }else if(material.getLength() >= length && material.getLength()-length <= 30){
+                return material;
+            }else if(material.getLength() >= length/2 && material.getLength()-(length/2) <= 30){
+                return material;
+            }else{
+                return null;
+            }
         }
-        return totalPrice;
+        return null;
     }
 
-    public void setTotalPrice(double totalPrice) {
+    public Map<Material,Integer> calculateMaterialList(ConnectionPool connectionPool) throws DatabaseException {
+        Map<Material,Integer> materialList = new HashMap<>();
+        List<Material> allMaterials = MaterialMapper.getAllMaterials(connectionPool);
+
+        //Laver en liste med spærtræ som skal bruges til remme og spær.
+        List<Material> spærMaterials = new ArrayList<>();
+        for(Material material:allMaterials){
+            if (material.getName().toLowerCase().contains("spær")){
+                spærMaterials.add(material);
+            }
+        }
+
+
+        //Først udregnes remme til carportens sider. Ved længder under 600 bruges én brædde til hele carportens længde.
+        //Ved længder over 600 cm bruges 2 i forlængelse af den mindst mulige der kan nå
+        if(length <= 600) {
+            materialList.put(getShortestSpærThatFits(spærMaterials, length), 2);
+        }else{
+            materialList.put(getShortestSpærThatFits(spærMaterials, length), 4);
+        }
+
+        //Så skal der regnes stolper. Vi går ud fra en maksimal aftsand på 300 cm mellem stolperne, med maks 100 cm fra enden
+        //materialId 12 er stolpen.
+        if(length <= 500) {
+            materialList.put(MaterialMapper.getMaterialById(12, connectionPool), 4);
+        }else{
+            materialList.put(MaterialMapper.getMaterialById(12, connectionPool), 6);
+        }
+
+        //Så mangler der kun spær. Vi går ud fra en afstand på 55 cm mellem spærene.
+        //Ved at caste længden til int, kan vi finde ud af hvor mange spær der skal bruges.
+        materialList.put(getShortestSpærThatFits(spærMaterials, width), ((int) length/55) + 1);
+        return materialList;
+
     }
 }
