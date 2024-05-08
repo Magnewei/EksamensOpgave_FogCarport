7c873ea752830c68bfe3efd837e225fbd8d8e521
Anton Stengaard <130669501+afstengaard@users.noreply.github.com>
2024-05-08 14:56:20 +0200

    Calculator done indtil videre (#94)

--- src/main/java/app/carport/Entities/Carport.java
+++ src/main/java/app/carport/Entities/Carport.java
@@ -58,6 +58,10 @@ public class Carport {
         return materialList;
     }
 
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
     public void setCarportID(int carportID) {
         this.carportID = carportID;
     }
@@ -78,8 +82,21 @@ public class Carport {
         this.materialList = materialList;
     }
 
-    public static Material getShortestSpærThatFits(List<Material> allMaterials, double length){
-        for(Material material : allMaterials){
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    //Vi tager 30% ekstra end materialeomkostninger i pris.
+    public double calculateTotalPrice(){
+        double totalPrice = 0;
+        for(Material material : materialList.keySet()){
+            totalPrice += material.getPrice() * materialList.get(material);
+        }
+        return totalPrice * 1.3;
+    }
+
+    public static Material getShortestWoodThatFits(List<Material> materials, double length){
+        for(Material material : materials){
             if(material.getLength() == length){
                 return material;
             }else if(material.getLength() >= length && material.getLength()-length <= 30){
@@ -88,6 +105,16 @@ public class Carport {
                 return material;
             }else{
                 return null;
+            }
+        }
+        return null;
+    }
+
+    //Tagpaldernes længder passer perfekt med de bredder vi tilbyder.
+    public static Material getShortestTagpladeThatFits(List<Material> materials, double length){
+        for(Material material : materials){
+            if(material.getLength() == length) {
+                return material;
             }
         }
         return null;
@@ -105,13 +132,29 @@ public class Carport {
             }
         }
 
+        //Laver en liste med sterntræ som skal bruges til remme og spær.
+        List<Material> sternMaterials = new ArrayList<>();
+        for(Material material:allMaterials){
+            if (material.getName().toLowerCase().contains("trykimp")){
+                spærMaterials.add(material);
+            }
+        }
+
+        //Laber en liste med tagplader.
+        List<Material> tagplader = new ArrayList<>();
+        for(Material material:allMaterials){
+            if (material.getName().toLowerCase().contains("trapezplade")){
+                tagplader.add(material);
+            }
+        }
+
 
         //Først udregnes remme til carportens sider. Ved længder under 600 bruges én brædde til hele carportens længde.
         //Ved længder over 600 cm bruges 2 i forlængelse af den mindst mulige der kan nå
         if(length <= 600) {
-            materialList.put(getShortestSpærThatFits(spærMaterials, length), 2);
+            materialList.put(getShortestWoodThatFits(spærMaterials, length), 2);
         }else{
-            materialList.put(getShortestSpærThatFits(spærMaterials, length), 4);
+            materialList.put(getShortestWoodThatFits(spærMaterials, length), 4);
         }
 
         //Så skal der regnes stolper. Vi går ud fra en maksimal aftsand på 300 cm mellem stolperne, med maks 100 cm fra enden
@@ -122,18 +165,46 @@ public class Carport {
             materialList.put(MaterialMapper.getMaterialById(12, connectionPool), 6);
         }
 
-        //Så mangler der kun spær. Vi går ud fra en afstand på 55 cm mellem spærene.
+        //Der skal også bruges sternbrædder. Der skal bruges en overstern og en understern, påsiderne og bagenden, men kun overstern foran
+        if(length <= 600) {
+            materialList.put(getShortestWoodThatFits(sternMaterials, length), 2);
+            materialList.put(getShortestWoodThatFits(sternMaterials, width), 1);
+        }else{
+            materialList.put(getShortestWoodThatFits(sternMaterials, length), 4);
+            materialList.put(getShortestWoodThatFits(sternMaterials, width), 1);
+        }
+
+
+        //Så mangler der kun spær. Vi går ud fra en afstand på 55 cm mellem spærene, og en spærbredde på 4.5 cm.
         //Ved at caste længden til int, kan vi finde ud af hvor mange spær der skal bruges.
-        materialList.put(getShortestSpærThatFits(spærMaterials, width), ((int) length/55) + 1);
+        int spærAmount = ((int) (length/(55+4.5)) + 1);
+        materialList.put(getShortestWoodThatFits(spærMaterials, width), spærAmount);
+
+        //Der skal også bruges tagplader. Tagpladerne lægges på tværs, og findes i de samme længder, som der er bredder tilgængelige.
+        //Tagpladerne er 109 cm brede, så vi sikrer at kunden får nok tagplader med, og så må kunden save til så det passer,
+        materialList.put(getShortestTagpladeThatFits(tagplader, length), (int) (width/109)+1);
+
+
+
+        //Det var alt træet, så mangler der skruer, beslag og lign.
+        //Vi starter med skruer til at montere spær på remmene. Der skal bruges to pr spær
+        materialList.put(MaterialMapper.getMaterialById(5, connectionPool), spærAmount*2);
+
+        //Der skal bruges 2 ruller hulbånd til et vindkryds.
+        materialList.put(MaterialMapper.getMaterialById(4, connectionPool), 2);
+
+        //Der skal også bruges en pakke skruer til at montere hulbåndet.
+        materialList.put(MaterialMapper.getMaterialById(2, connectionPool), 1);
+
+        //Der skal også bruges skruer til at montere tagplader. Der skal bruges 12 pr m2, og de kommer i pakker af 250.
+        materialList.put(MaterialMapper.getMaterialById(3, connectionPool), (int) ((length*width/10000)*12/200)+1);
+
+        //Og en pakke skruer til sterntræet
+        materialList.put(MaterialMapper.getMaterialById(1, connectionPool), 1);
+
         return materialList;
 
     }
 
-    public double getTotalPrice() {
-        return totalPrice;
-    }
 
-    public void setTotalPrice(double totalPrice) {
-        this.totalPrice = totalPrice;
-    }
 }
