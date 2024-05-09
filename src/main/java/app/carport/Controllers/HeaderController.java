e5e99995a20385ce2467c5ad0dc4f28a70620c23
Magnewei <magnewei@icloud.com>
2024-05-09 17:36:00 +0200

    Small refactoring Added class diagram v4

--- src/main/java/app/carport/Controllers/HeaderController.java
+++ src/main/java/app/carport/Controllers/HeaderController.java
@@ -1,9 +1,6 @@
 package app.carport.Controllers;
 
-import app.carport.Entities.User;
-import app.carport.Exceptions.DatabaseException;
 import app.carport.Persistence.ConnectionPool;
-import app.carport.Persistence.UserMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
@@ -11,36 +8,23 @@ public class HeaderController {
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
         app.post("loadadmin", ctx -> goToAdmin(connectionPool, ctx));
         app.post("goToLogin", ctx -> goToLogin(ctx));
-        app.post("goToOrdrer", ctx -> goToOrdrer(ctx, connectionPool));
+        app.post("goToOrdrer", ctx -> goToOrder(ctx, connectionPool));
         app.post("goToIndex", ctx -> goToIndex(ctx));
     }
 
     private static void goToAdmin(ConnectionPool connectionPool, Context ctx) {
-        try {
-            AdminPanelController.renderAdmin(connectionPool,ctx);
-        } catch (NumberFormatException e) {
-            ctx.attribute("message", e.getMessage());
-            ctx.render("admin.html");
-        }
+            AdminPanelController.renderAdmin(connectionPool, ctx);
     }
 
     public static void goToLogin(Context ctx) {
-
-            ctx.render("login.html");
-        }
-
-
-    public static void goToOrdrer(Context ctx, ConnectionPool connectionPool) {
-        try {
-            UserController.renderOrdrer(ctx, connectionPool);
-        } catch (NumberFormatException e) {
-            ctx.attribute("message", e.getMessage());
-            ctx.render("ordrer.html");
-        }
+        ctx.render("login.html");
     }
 
+    public static void goToOrder(Context ctx, ConnectionPool connectionPool) {
+            UserController.renderOrder(ctx, connectionPool);
+    }
 
     public static void goToIndex(Context ctx) {
-           ctx.render("index.html");
-        }
+        ctx.render("index.html");
     }
+}
