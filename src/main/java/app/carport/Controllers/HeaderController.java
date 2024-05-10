a1f935af370a4bf610cddc13842941e6d6822515
Magi2106 <144230160+Magi2106@users.noreply.github.com>
2024-05-10 03:17:05 +0200

    CreateUser og lidt bonus css + fragments fix og lidt mere.

--- src/main/java/app/carport/Controllers/HeaderController.java
+++ src/main/java/app/carport/Controllers/HeaderController.java
@@ -7,13 +7,15 @@ import app.carport.Persistence.UserMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
+import static app.carport.Controllers.UserController.logout;
+
 public class HeaderController {
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
         app.post("loadadmin", ctx -> goToAdmin(connectionPool, ctx));
         app.post("goToLogin", ctx -> goToLogin(ctx));
         app.post("goToOrdrer", ctx -> goToOrdrer(ctx, connectionPool));
         app.post("goToIndex", ctx -> goToIndex(ctx));
-
+        app.post("/logout", ctx -> logout(ctx));
     }
 
     private static void goToAdmin(ConnectionPool connectionPool, Context ctx) {
