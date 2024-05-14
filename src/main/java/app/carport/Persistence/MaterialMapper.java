57669ab2126c6a98de22fce3dda19163805f2dec
Magnewei <Magnewei@Icloud.com>
2024-05-14 15:29:58 +0200

    MaterialMapper docs

--- src/main/java/app/carport/Persistence/MaterialMapper.java
+++ src/main/java/app/carport/Persistence/MaterialMapper.java
@@ -10,8 +10,17 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Provides database operations for managing materials within the application.
+ */
 public class MaterialMapper {
 
+    /**
+     * Retrieves a list of all distinct lengths of carports stored in the database.
+     * @param connectionPool Connection pool for database connections.
+     * @return A list of all distinct lengths.
+     * @throws DatabaseException If there is a problem executing the query.
+     */
     public static List<Double> getAllLength(ConnectionPool connectionPool) throws DatabaseException {
         List<Double> LengthList = new ArrayList<>();
         String sql = "SELECT DISTINCT length FROM carport ORDER BY length;";
@@ -27,7 +36,12 @@ public class MaterialMapper {
         return LengthList;
     }
 
-
+    /**
+     * Retrieves a list of all distinct widths of carports stored in the database.
+     * @param connectionPool Connection pool for database connections.
+     * @return A list of all distinct widths.
+     * @throws DatabaseException If there is a problem executing the query.
+     */
     public static List<Double> getAllWidth(ConnectionPool connectionPool) throws DatabaseException {
         List<Double> WidthList = new ArrayList<>();
         String sql = "SELECT DISTINCT width FROM carport ORDER BY width;";
@@ -43,6 +57,12 @@ public class MaterialMapper {
         return WidthList;
     }
 
+    /**
+     * Retrieves a list of all materials available in the database.
+     * @param connectionPool Connection pool for database connections.
+     * @return A list of materials.
+     * @throws DatabaseException If there is a problem executing the query.
+     */
     public static List<Material> getAllMaterials(ConnectionPool connectionPool) throws DatabaseException {
         List<Material> materialList = new ArrayList<>();
         String sql = "select * from material";
@@ -63,6 +83,13 @@ public class MaterialMapper {
         return materialList;
     }
 
+    /**
+     * Deletes a material by its ID from the database.
+     * @param connectionPool Connection pool for database connections.
+     * @param materialID The ID of the material to delete.
+     * @return true if the deletion was successful, false otherwise.
+     * @throws DatabaseException If there is a problem executing the delete operation.
+     */
     public static boolean deleteMaterialById(ConnectionPool connectionPool, int materialID) throws DatabaseException {
         String sql = "DELETE FROM material WHERE \"materialID\" = ?";
 
@@ -76,6 +103,13 @@ public class MaterialMapper {
         }
     }
 
+    /**
+     * Retrieves a specific material by its ID from the database.
+     * @param materialId The ID of the material to retrieve.
+     * @param connectionPool Connection pool for database connections.
+     * @return The retrieved Material object, or null if not found.
+     * @throws DatabaseException If there is a problem executing the query.
+     */
     public static Material getMaterialById(int materialId, ConnectionPool connectionPool) throws DatabaseException {
         String sql = "SELECT * FROM material WHERE \"materialID\" = ?";
 
@@ -96,6 +130,17 @@ public class MaterialMapper {
         return null;
     }
 
+    /**
+     * Adds a new material to the database.
+     * @param connectionPool Connection pool for database connections.
+     * @param name Name of the material.
+     * @param price Price of the material.
+     * @param length Length of the material.
+     * @param unit Unit of measurement for the material.
+     * @param quantityInStock Quantity of the material in stock.
+     * @return true if the material was added successfully, false otherwise.
+     * @throws DatabaseException If there is a problem executing the insert operation.
+     */
     public static boolean addMaterial(ConnectionPool connectionPool, String name, double price, double length, String unit, int quantityInStock) throws DatabaseException {
         String sql = "INSERT INTO material (name, price, length, unit, \"quantityInStock\") VALUES (?, ?, ?, ?, ?)";
         try (Connection connection = connectionPool.getConnection(); PreparedStatement ps = connection.prepareStatement(sql)) {
