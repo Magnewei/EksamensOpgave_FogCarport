472d815047d44118282232e0756a5c9c0ef43d8b
Anton Stengaard <afstengaard@gmail.com>
2024-05-16 10:49:08 +0200

    Navigationsdiagram

--- src/main/java/app/carport/Persistence/CarportMapper.java
+++ src/main/java/app/carport/Persistence/CarportMapper.java
@@ -67,7 +67,7 @@ public class CarportMapper {
         }
         return 0;
     }
-
+    //Blev brugt til at populate db
     public static void addAllPossibleMaterialsToDb(ConnectionPool connectionPool) throws DatabaseException {
         String sql = "INSERT INTO \"materialUsage\" (\"carportID\", \"materialID\", \"quantity\") VALUES (?,?,?)";
         double[] possibleLengths = {420, 480, 540, 600, 660, 720, 780}; // replace with your possible lengths
