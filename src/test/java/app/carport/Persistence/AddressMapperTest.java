36112fd1bccafb00b24b54138c79f986a393a35d
Magnewei <Magnewei@Icloud.com>
2024-05-17 15:29:49 +0200

    Added test stuff

--- src/test/java/app/carport/Persistence/AddressMapperTest.java
+++ src/test/java/app/carport/Persistence/AddressMapperTest.java
@@ -1,6 +1,5 @@
 package app.carport.Persistence;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,22 +8,31 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class AddressMapperTest {
 
-    private final static String DB_SCHEMA = "fogprojekt_test";
-
-    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(
-            System.getenv("JDBC_USER"),
-            System.getenv("JDBC_PASSWORD"),
-            System.getenv("JDBC_CONNECTION_STRING"),
-            DB_SCHEMA);
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance();
 
     @BeforeAll
     static void setupClass() throws SQLException {
         try (Connection connection = connectionPool.getConnection()) {
-            try (Statement statement = connection.createStatement()) {
+            try (Statement stmt = connection.createStatement()) {
+                // The test schema is already created, so we only need to delete/create test tables
+                stmt.execute("DROP TABLE IF EXISTS public.users");
+                stmt.execute("DROP TABLE IF EXISTS public.orders");
+                stmt.execute("DROP SEQUENCE IF EXISTS public.users_user_id_seq CASCADE;");
+                stmt.execute("DROP SEQUENCE IF EXISTS public.orders_order_id_seq CASCADE;");
+
+                // Create tables as copy of original public schema structure
+                stmt.execute("CREATE TABLE public.users AS (SELECT * from public.users) WITH NO DATA");
+                stmt.execute("CREATE TABLE test.orders AS (SELECT * from public.orders) WITH NO DATA");
+
+                // Create sequences for auto generating id's for users and orders
+                stmt.execute("CREATE SEQUENCE test.users_user_id_seq");
+                stmt.execute("ALTER TABLE test.users ALTER COLUMN user_id SET DEFAULT nextval('test.users_user_id_seq')");
+                stmt.execute("CREATE SEQUENCE test.orders_order_id_seq");
+                stmt.execute("ALTER TABLE test.orders ALTER COLUMN order_id SET DEFAULT nextval('test.orders_order_id_seq')");
 
             } catch (SQLException e) {
                 e.printStackTrace();
@@ -37,7 +45,7 @@ class AddressMapperTest {
     @BeforeEach
     void setUp() {
         try (Connection connection = connectionPool.getConnection()) {
-            try (Statement statement = connection.createStatement()) {
+            try (Statement stmt = connection.createStatement()) {
 
 
             } catch (SQLException e) {
