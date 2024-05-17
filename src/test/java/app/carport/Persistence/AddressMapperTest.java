7cdb2242e2c258b0bc7fb8ee0bb0ed7a4637d230
Magnewei <Magnewei@Icloud.com>
2024-05-17 13:03:46 +0200

    Fixed up final class diagram

--- src/test/java/app/carport/Persistence/AddressMapperTest.java
+++ src/test/java/app/carport/Persistence/AddressMapperTest.java
@@ -1,20 +1,55 @@
 package app.carport.Persistence;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AddressMapperTest {
 
-    @BeforeEach
-    void setUp() {
+    private final static String DB_SCHEMA = "fogprojekt_test";
+
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(
+            System.getenv("JDBC_USER"),
+            System.getenv("JDBC_PASSWORD"),
+            System.getenv("JDBC_CONNECTION_STRING"),
+            DB_SCHEMA);
+
+    @BeforeAll
+    static void setupClass() throws SQLException {
+        try (Connection connection = connectionPool.getConnection()) {
+            try (Statement statement = connection.createStatement()) {
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                fail("The connection to the database failed.");
+            }
+        }
     }
 
-    @AfterEach
-    void tearDown() {
+
+    @BeforeEach
+    void setUp() {
+        try (Connection connection = connectionPool.getConnection()) {
+            try (Statement statement = connection.createStatement()) {
+
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                fail("The connection to the database failed.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            fail("The connection to the database failed.");
+        }
     }
+
 
     @Test
     void insertAddress() {
